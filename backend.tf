e10db38c10aabab55de0aead849a1d80fd389ca9
sohanso <109266261+sohanso@users.noreply.github.com>
2022-08-04 10:16:37 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "talacademy-so1-tflab-bucket"
-        key = "talent-academy/ec2/terraform.tfstate"
-        region = "eu-west-1"
-        dynamodb_table = "terraform-lock"
-    }
-}
